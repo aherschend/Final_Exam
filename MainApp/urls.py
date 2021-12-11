0f03e3d5598d687d2ac11652b97f22ece8f100aa
Annabelle Herschend <annabelle_herschend1@baylor.edu>
2021-12-11 17:25:15 -0600

    Added Customer Comment Section

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('pizzas', views.pizzas, name='pizzas'),
     path('pizza/<int:pizza_id>/', views.pizza, name='pizza'),
-    path('new_comment/<int:pizza_id/',views.new_comment, name='new_comment')
+    path('new_comment/<int:pizza_id>/',views.new_comment, name='new_comment')
 ]
 
