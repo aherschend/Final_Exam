a335b424cacfc7b9448dd3a9f6a8e6d39a129525
Annabelle Herschend <annabelle_herschend1@baylor.edu>
2021-12-12 21:27:46 -0600

    Added Images

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Pizza, Topping, Comment
+from .models import Pizza, Topping, Comment, Image
 
 admin.site.register(Pizza)
 admin.site.register(Topping)
 admin.site.register(Comment)
+admin.site.regiser(Image)
